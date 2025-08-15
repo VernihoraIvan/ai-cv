6230c2fe68ae00ae2c69391aba97c538f4ae0438
Ivan Vernihora <vernihorai@gmail.com>
2025-08-15 20:15:39 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   basePath: "/ad-cv-nextjs",
   assetPrefix: "/ad-cv-nextjs",
-  output: "export",
 };
 
 export default nextConfig;
