000246c21c0508ac759608fea5816c900a9a0fe2
Ivan Vernihora <vernihorai@gmail.com>
2025-08-16 14:50:16 +0200

    Enable redirect from Home component to /chat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
-// import { redirect } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  // redirect("/chat");
-  return <div>Hello</div>;
+  redirect("/chat");
 }
