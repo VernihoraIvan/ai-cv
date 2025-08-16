5198b84e250a7dfbf75e07b7a344f45b07c87268
Ivan Vernihora <vernihorai@gmail.com>
2025-08-16 13:17:13 +0200

    Refactor Home component to initialize Chat with empty messages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,21 +2,15 @@ export const runtime = "edge";
 
 import Chat from "@/components/Chat";
 import { createNewSession } from "@/lib/session";
-import { getChatMessages } from "@/lib/queries/chat_messages";
-
-type Message = {
-  content: string;
-  role: "user" | "assistant";
-};
 
 export default async function Home() {
   const sessionId = await createNewSession();
-  const initialMessages = (await getChatMessages(sessionId)) as Message[];
+  // const initialMessages = (await getChatMessages(sessionId)) as Message[];
 
   return (
     <main>
       <div className="w-full h-full">
-        <Chat initialMessages={initialMessages || []} sessionId={sessionId} />
+        <Chat initialMessages={[]} sessionId={sessionId} />
       </div>
     </main>
   );
